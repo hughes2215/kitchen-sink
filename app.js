263f3a91f3b75a347c82a9bdcb5f9de2cf5ce63a
hughes2215 <hughes2215@gmail.com>
2018-06-05 15:13:53 -0500

    lastest work

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var name = 'Billy';
+var name = "Billy";
 
 const numberofstates = 50;
+
+var equals = 5 + 4;
+//this is the alert tha displays hello world
+function sayHello(){
+    alert("Hello World");
+}
+//this calls hello world alert
+console.log(sayHello);
